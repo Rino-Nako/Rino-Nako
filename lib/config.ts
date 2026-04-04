0237c6c792eead0b0a10f45317f5510c08c68f46
nako-arkrin <jessegary.lab@outlook.com>
2026-04-05 02:47:20 +0800

    feat: 隐藏了不需要的功能

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
-export const HIDEEX_FUNCTION = false; // If true, hides plugin settings
-export const HIDE_BETAFUN = false;    // If true, hides experimental features
-export const HIDE_CLOUDSYNC = false;  // If true, hides cloud sync function
+export const HIDEEX_FUNCTION = true; // If true, hides plugin settings
+export const HIDE_BETAFUN = true;    // If true, hides experimental features
+export const HIDE_CLOUDSYNC = true;  // If true, hides cloud sync function
